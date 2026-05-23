deaa57f133bd6a1ef5fcf886733b25057375d5bd
Mahbub Hasan <easycodermahbubwith5500@gmail.com>
2026-05-23 16:28:48 +0600

    code modified

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import { authRoute } from "./modules/auth/auth.route";
 import { issueRoute } from "./modules/issue/issue.route";
+import cors from "cors";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost: 3000" }));
 
-app.get("/", (req, res) => {
-  res.send("DevPulse - Assignment");
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Express Server",
+    author: "Mahbub Hasan",
+  });
 });
 
 app.use("/api/auth", authRoute);
 app.use("/api/issues", issueRoute);
+app.use(globalErrorHandler);
 
 export default app;
